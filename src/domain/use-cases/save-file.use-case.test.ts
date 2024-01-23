82c1edc28daf32fabba1522fe4b3d20bd036fcb4
Dev19Carmona <camilocr3@hotmail.com>
2024-01-23 17:54:49 -0500

    Pruebas - SaveFile UseCase custom

---
+++ src/domain/use-cases/save-file.use-case.test.ts
@@ -0,0 +1,23 @@
+import { SaveFile } from './save-file.use-case'
+import fs from 'fs'
+describe('save-file.use-case', () => {
+  afterEach(() => {
+    fs.rmSync('outputs', { recursive: true })
+  })
+  test('should save file with default values', () => {
+    const filePath = 'outputs/table.txt'
+    const saveFile = new SaveFile()
+    const options = {
+      fileContent: 'test content',
+    }
+    const result = saveFile.execute(options)
+    const fileExist = fs.existsSync(filePath)
+    const fileContent = fs.readFileSync(filePath, {
+      encoding: 'utf-8',
+    })
+
+    expect(result).toBe(true)
+    expect(fileExist).toBe(true)
+    expect(fileContent).toBe(options.fileContent)
+  })
+})
